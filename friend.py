734a787d6f0e4622663ded91a302866b27d5baf0
princewillzz <princewillz2013@gmail.com>
2020-04-05 20:37:00 +0530

    few functionality added like clean

--- friend.py
+++ friend.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
 import os
 import sys
 
 def remove_unwanted(name_of_file):
-    
+    files = os.listdir()
+    to_be_delete = list()
+    for file in files:
+        splited_file = file.split(".")
+        if splited_file[len(splited_file)-1] == "class":
+            to_be_delete.append(file)
+
+    for file in to_be_delete:
+        os.remove(file)
 
 
 def writeContent(name_of_file):
     f2 = open(name_of_file, "w+")
-    f1 = open("forjava.txt")
+    f1 = open("D:\projects\my_code_helper/forjava.txt")
 
 
     for content in f1:
@@ -25,4 +34,3 @@ if __name__ == "__main__":
         remove_unwanted(name_of_file)
     else:
         writeContent(name_of_file)
-
