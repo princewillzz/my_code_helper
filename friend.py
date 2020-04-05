726a054c8dfe45ff9854bb49dd465a9d78850829
princewillzz <princewillz2013@gmail.com>
2020-04-06 00:20:37 +0530

    Added some features like compile and run, giving input without using .extension and removing the unwanted .class type of extension

--- friend.py
+++ friend.py
@@ -2,24 +2,63 @@
 import os
 import sys
 
-def remove_unwanted(name_of_file):
+# Compile and Run the program given as the input 
+def doit():
+    # Access the file name to be executed
+    f = open("D:\projects\my_code_helper/run.txt")
+    content = f.readline()
+
+    # Return if there exists no files stored in the "run.txt" file to be executed
+    if content == "":
+        return
+    try:
+        if os.system("javac " + content) == 1:
+            return
+        cont = content.split(".")
+        os.system("java " + cont[0])
+
+    except:
+        print("except")
+        f.close()
+        return 
+
+    f.close()
+    # To remove the ".class" type file again
+    remove_unwanted()
+
+
+# Remove the ".class" type file created after the execution of the program 
+def remove_unwanted():
     files = os.listdir()
     to_be_delete = list()
+    # Store all the files to be deleted
     for file in files:
         splited_file = file.split(".")
         if splited_file[len(splited_file)-1] == "class":
             to_be_delete.append(file)
-
+    # remove all the files that are stored 
     for file in to_be_delete:
         os.remove(file)
 
 
+# Write the contents from "forjava.txt" to the "name_of_file" file
 def writeContent(name_of_file):
-    f2 = open(name_of_file, "w+")
+    # Write the name of the file in run.txt to compile and run the program later
+    f = open("D:\projects\my_code_helper/run.txt", "w")
+    f.write(name_of_file)
+    f.close()
+
+    # Check if the file already exist then just compile and run the program
+    if name_of_file in os.listdir():
+        doit()
+        return
+    # if the file is not present create a new file with the name given as input
+    f2 = open(name_of_file, "w+") 
     f1 = open("D:\projects\my_code_helper/forjava.txt")
-
-
+    
+    # Do the actual stuff of wrting the pre-set code into the new file
     for content in f1:
+        # To replace the class name from "forjava" --> name_of_file.java
         if "forjava" in content:
             name_of_file = name_of_file.split(".")
             cont = content.replace("forjava", name_of_file[0])
@@ -27,10 +66,16 @@ def writeContent(name_of_file):
         else:
             f2.write(content)
 
+    f1.close()
+    f2.close()
+
 
 if __name__ == "__main__":
     name_of_file = sys.argv[1]
     if name_of_file == "clean":
-        remove_unwanted(name_of_file)
+        remove_unwanted()
+    elif name_of_file == "doit":
+        doit()
     else:
+        name_of_file += ".java"
         writeContent(name_of_file)
