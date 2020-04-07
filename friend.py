4ece96357d4b7ce16dbc82300f59f7fed0cf75e7
princewillzz <princewillz2013@gmail.com>
2020-04-08 00:47:35 +0530

    after implementing voice

--- friend.py
+++ friend.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
-import os
+import os, datetime
 import sys
+import speech_recognition as sr 
+import pyttsx3 
+
+# Properties of the python text to speech engine
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 140)
+
+# Properties of my Recognizer
+r = sr.Recognizer()
+r.pause_threshold = 0.5
+r.energy_threshold = 100
+
+
 
 # Compile and Run the program given as the input 
 def doit():
@@ -67,30 +82,87 @@ def writeContent(name_of_file):
     f1.close()
     f2.close()
 
+# Integrating it with git VC
 def git_add(message):
     remove_unwanted()
     os.system("git add .")
     os.system("git commit -m " + "\"" + message + "\"")
+    os.system("git push")
+
+
+def speak(audio):
+    try:
+        engine.say(audio)
+        engine.runAndWait()
+        return 0
+    except:
+        print("Sorry, for inconvenience")
+    return 1
+
+
+def wish():
+    time_in_hour = datetime.datetime.now()
+    if time_in_hour > 4 and time_in_hour < 12 :
+        speak("Good, Morning! my love")
+    elif time_in_hour < 17:
+        speak("Good, Afternoon! my love")
+    else :
+        speak("Good evening! Love")
+
+def search_name(audio_message):
+    words = audio_message.split(" ")
+    return words[len(words)-1]
+
+
+
+def voice_input():
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Listening")
+        audio = r.listen(source, timeout=6, phrase_time_limit=4)
+
+    try:
+        response = r.recognize_google(audio)
+        return response
+    except sr.UnknownValueError:
+        print("Sorry, I was not able to understand")
+    except sr.WaitTimeoutError:
+        print("Time was over")
+    except sr.RequestError:
+        print("Can't reach the recognizer")
+
+    return None
+
 
 
 def main():
-    name_of_file = sys.argv[1]
-    if name_of_file == "clean":
-        remove_unwanted()
-    elif name_of_file == "doit":
-        doit()
-    elif name_of_file == "commit":
-        try:
-            message = input("Message:- ")
-            if message.strip() == "":
-                print("cannot commit without message:)")
+    while True:
+        command = voice_input().lower()
+        if command == None: 
+            voice_input()
+
+        if "clean" in command:
+            remove_unwanted()
+        elif "run" in command:
+            doit()
+        elif "commit" in command:
+            try:
+                message = input("Message:- ")
+                if message.strip() == "":
+                    print("cannot commit without message:)")
+                    return
+                git_add(message)
+            except:
                 return
-            git_add(message)
-        except:
-            return
-    else:
-        name_of_file += ".java"
-        writeContent(name_of_file)
+        elif "create" in command:
+            speak("Are you sure to create the file with this name: ")
+            name_of_file = search_name(command)
+            name_of_file += ".java"
+            writeContent(name_of_file)
+
+
+
 
 if __name__ == "__main__":
+    wish()
     main()
