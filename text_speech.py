565ced2308ae188f453f63314a2a4f90f5698c84
princewillzz <princewillz2013@gmail.com>
2020-04-07 23:27:38 +0530

    text to speech

--- text_speech.py
+++ text_speech.py
@@ -2,10 +2,11 @@ import pyttsx3
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
+voices[1].age = 5
+print(voices[1].age, voices[1].gender)
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 140)
 
 
-engine.connect('finished-utterance', onEnd)
 engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
