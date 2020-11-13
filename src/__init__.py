9167a8b17b0b80fb4be650945eb800629723f0f3
princewillzz <princewillz2013@gmail.com>
2020-11-13 17:06:52 +0530

    implemented the image to pdf converted app using tkinter

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,10 @@ from .javawork import Create_or_run_java_file, run_java
 from .cwork import create_or_run_c_file, run_c
 from .features import *
 from .pdfreader import readPDF
-from .convertdp import *
+from .convertdocx2pdf import *
 from .convertPDFToDocx import *
 from .sqlwork import generateSQLQueries
 
 from .menu import displayHelp
+
+from .ImageToPDFConverterAPP import ImageToPDFConverterAPP, runImageToPDFConverterApp
